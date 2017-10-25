caa489cd060f35aeb0330911333abb86c6c3536f
Nicolas Trangez <ikke@nicolast.be>
2017-10-26 01:23:50 +0200

    Optimize `Tag` type
    Instead of using a record with multiple `Maybe` fields and the potential
    of generating (morally) invalid values, use a proper ADT. Reduced memory
    usage and increased encoding/decoding performance come for free.

--- jaeger-types/src/Jaeger/Types/Tag.hs
+++ jaeger-types/src/Jaeger/Types/Tag.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
 {-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
 
@@ -15,20 +17,17 @@ module Jaeger.Types.Tag (
 
 import Data.Int (Int64)
 import GHC.Generics (Generic)
-import GHC.Stack (HasCallStack)
 
 import Data.ByteString (ByteString)
 
 import Data.Text (Text)
 
-import Control.Lens (
-    Getter, Getting, Lens', Prism',
-    (&), (^.), (.~), (?~),
-    makeLenses, prism, review)
+import Control.Lens (Getter, Lens', lens, makePrisms, to)
 
 import Control.DeepSeq (NFData)
 
-import Pinch (Enumeration, Field, Pinchable, enum, field, putField)
+import Pinch (Enumeration, Pinchable, TStruct, (.=), (.:), enum, struct)
+import qualified Pinch
 
 -- | 'TagType' denotes the type of a 'Tag's value.
 --
@@ -71,37 +70,76 @@ binary = BINARY enum
 -- >   6: optional i64     vLong
 -- >   7: optional binary  vBinary
 -- > }
-data Tag = Tag { _tagKey' :: !(Field 1 Text)
-               , _tagVType' :: !(Field 2 TagType)
-               , _tagVStr' :: !(Field 3 (Maybe Text))
-               , _tagVDouble' :: !(Field 4 (Maybe Double))
-               , _tagVBool' :: !(Field 5 (Maybe Bool))
-               , _tagVLong':: !(Field 6 (Maybe Int64))
-               , _tagVBinary' :: !(Field 7 (Maybe ByteString))
-               }
+data Tag = StringTag !Text !Text
+         | DoubleTag !Text !Double
+         | BoolTag !Text !Bool
+         | LongTag !Text !Int64
+         | BinaryTag !Text !ByteString
     deriving (Show, Eq, Generic)
 
-makeLenses ''Tag
+makePrisms ''Tag
 instance NFData Tag
-instance Pinchable Tag
+
+instance Pinch.Pinchable Tag where
+    type Tag Tag = TStruct
+
+    pinch = \case
+        StringTag k v -> struct [ 1 .= k
+                                , 2 .= string
+                                , 3 .= v
+                                ]
+        DoubleTag k v -> struct [ 1 .= k
+                                , 2 .= double
+                                , 4 .= v
+                                ]
+        BoolTag k v -> struct [ 1 .= k
+                              , 2 .= bool
+                              , 5 .= v
+                              ]
+        LongTag k v -> struct [ 1 .= k
+                              , 2 .= long
+                              , 6 .= v
+                              ]
+        BinaryTag k v -> struct [ 1 .= k
+                                , 2 .= binary
+                                , 7 .= v
+                                ]
+    unpinch value = do
+        typ <- value .: 2
+        case typ of
+            STRING _ -> StringTag <$> value .: 1 <*> value .: 3
+            DOUBLE _ -> DoubleTag <$> value .: 1 <*> value .: 4
+            BOOL _ -> BoolTag <$> value .: 1 <*> value .: 5
+            LONG _ -> LongTag <$> value .: 1 <*> value .: 6
+            BINARY _ -> BinaryTag <$> value .: 1 <*> value .: 7
 
 -- | 'Tag' @key@.
 tagKey :: Lens' Tag Text
-tagKey = tagKey' . field
+tagKey = lens get set
+  where
+    get = \case
+        StringTag k _ -> k
+        DoubleTag k _ -> k
+        BoolTag k _ -> k
+        LongTag k _ -> k
+        BinaryTag k _ -> k
+    set tag k = case tag of
+        StringTag _ v -> StringTag k v
+        DoubleTag _ v -> DoubleTag k v
+        BoolTag _ v -> BoolTag k v
+        LongTag _ v -> LongTag k v
+        BinaryTag _ v -> BinaryTag k v
 {-# INLINE tagKey #-}
 
 -- | 'Tag' @vType@.
---
--- /Note:/ `tagType` is 'Getter', /not/ a 'Lens', otherwise it'd be too easy
--- to create invalid 'Tag's where the field corresponding with the 'TagType'
--- isn't set.
 tagType :: Getter Tag TagType
-tagType = tagVType' . field
+tagType = to $ \case
+    StringTag{} -> string
+    DoubleTag{} -> double
+    BoolTag{} -> bool
+    LongTag{} -> long
+    BinaryTag{} -> binary
 {-# INLINE tagType #-}
-
-fromTagUnion :: HasCallStack => Getting a (Maybe a) a
-fromTagUnion f = maybe (error "Tag: mismatch between vType and optional fields") (fmap Just . f)
-{-# INLINE fromTagUnion #-}
 
 -- | Map a family of functions over the value carried in a 'Tag'.
 mapTagValue :: (Text -> a)  -- ^ Handle a 'Text' payload
@@ -111,102 +149,34 @@ mapTagValue :: (Text -> a)  -- ^ Handle a 'Text' payload
             -> (ByteString -> a)  -- ^ Handle a 'ByteStrin' payload
             -> Tag  -- ^ 'Tag' whose value to map over
             -> a
-mapTagValue t d b i b' tag = case tag ^. tagVType' . field of
-    STRING _ -> t (tag ^. tagVStr' . field . fromTagUnion)
-    DOUBLE _ -> d (tag ^. tagVDouble' . field . fromTagUnion)
-    BOOL _ -> b (tag ^. tagVBool' . field . fromTagUnion)
-    LONG _ -> i (tag ^. tagVLong' . field . fromTagUnion)
-    BINARY _ -> b' (tag ^. tagVBinary' . field . fromTagUnion)
-
--- Note: This is a placeholder, but actually an invalid Tag.
-emptyTag :: Tag
-emptyTag = Tag { _tagKey' = putField mempty
-               , _tagVType' = putField string
-               , _tagVStr' = putField Nothing
-               , _tagVDouble' = putField Nothing
-               , _tagVBool' = putField Nothing
-               , _tagVLong' = putField Nothing
-               , _tagVBinary' = putField Nothing
-               }
-{-# INLINE emptyTag #-}
-
--- | 'Prism' for a 'Tag' carrying 'Text'.
-_StringTag :: Prism' Tag (Text, Text)
-_StringTag = prism wrap extract
-  where
-    wrap (k, v) = emptyTag & tagKey .~ k
-                           & tagVType' . field .~ string
-                           & tagVStr' . field ?~ v
-    extract t = case t ^. tagVType' . field of
-        STRING _ -> Right (t ^. tagKey, t ^. tagVStr' . field . fromTagUnion)
-        _ -> Left t
-
--- | 'Prism' for a 'Tag' carrying a 'Double'.
-_DoubleTag :: Prism' Tag (Text, Double)
-_DoubleTag = prism wrap extract
-  where
-    wrap (k, v) = emptyTag & tagKey .~ k
-                           & tagVType' . field .~ double
-                           & tagVDouble' . field ?~ v
-    extract t = case t ^. tagVType' . field of
-        DOUBLE _ -> Right (t ^. tagKey, t ^. tagVDouble' . field . fromTagUnion)
-        _ -> Left t
-
--- | 'Prism' for a 'Tag' carrying a 'Bool'.
-_BoolTag :: Prism' Tag (Text, Bool)
-_BoolTag = prism wrap extract
-  where
-    wrap (k, v) = emptyTag & tagKey .~ k
-                           & tagVType' . field .~ bool
-                           & tagVBool' . field ?~ v
-    extract t = case t ^. tagVType' . field of
-        BOOL _ -> Right (t ^. tagKey, t ^. tagVBool' . field . fromTagUnion)
-        _ -> Left t
-
--- | 'Prism' for a 'Tag' carrying an 'Int64'.
-_LongTag :: Prism' Tag (Text, Int64)
-_LongTag = prism wrap extract
-  where
-    wrap (k, v) = emptyTag & tagKey .~ k
-                           & tagVType' . field .~ long
-                           & tagVLong' . field ?~ v
-    extract t = case t ^. tagVType' . field of
-        LONG _ -> Right (t ^. tagKey, t ^. tagVLong' . field . fromTagUnion)
-        _ -> Left t
-
--- | 'Prism' for a 'Tag' carrying a 'ByteString'.
-_BinaryTag :: Prism' Tag (Text, ByteString)
-_BinaryTag = prism wrap extract
-  where
-    wrap (k, v) = emptyTag & tagKey .~ k
-                           & tagVType' . field .~ binary
-                           & tagVBinary' . field ?~ v
-    extract t = case t ^. tagVType' . field of
-        BINARY _ -> Right (t ^. tagKey, t ^. tagVBinary' . field . fromTagUnion)
-        _ -> Left t
-
+mapTagValue t d b i b' = \case
+    StringTag _ v -> t v
+    DoubleTag _ v -> d v
+    BoolTag _ v -> b v
+    LongTag _ v -> i v
+    BinaryTag _ v -> b' v
 
 -- | Construct a 'Text' 'Tag'.
 stringTag :: Text -> Text -> Tag
-stringTag = curry (review _StringTag)
+stringTag = StringTag
 {-# INLINE stringTag #-}
 
 -- | Construct a 'Double' 'Tag'.
 doubleTag :: Text -> Double -> Tag
-doubleTag = curry (review _DoubleTag)
+doubleTag = DoubleTag
 {-# INLINE doubleTag #-}
 
 -- | Construct a 'Bool' 'Tag'.
 boolTag :: Text -> Bool -> Tag
-boolTag = curry (review _BoolTag)
+boolTag = BoolTag
 {-# INLINE boolTag #-}
 
 -- | Construct an 'Int64' 'Tag'.
 longTag :: Text -> Int64 -> Tag
-longTag = curry (review _LongTag)
+longTag = LongTag
 {-# INLINE longTag #-}
 
 -- | Construct a 'ByteString' 'Tag'.
 binaryTag :: Text -> ByteString -> Tag
-binaryTag = curry (review _BinaryTag)
+binaryTag = BinaryTag
 {-# INLINE binaryTag #-}
